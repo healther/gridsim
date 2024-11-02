825ad4f8c41bf9fadd7eb3b7ef6095ef09dd516d
Andreas Baumbach <healther.astro@gmail.com>
2024-11-02 13:09:07 +0100

    Rework grid.run_simulation
    remove the old extra capacity hack and clean up the capacity scaling

---
+++ grid/capacity.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class PowerCapacity:
+    def __init__(self, historic_capacity, simulated_capacity):
+        self.historic = {}
+        for name, sourcedict in historic_capacity.items():
+            times = []
+            values = []
+            for t, v in sourcedict.items():
+                times.append(t)
+                values.append(v)
+            times = np.array(times)
+            values = np.array(values)
+            inds = np.argsort(times)
+
+            self.historic[name] = {
+                "times": times[inds],
+                "values": values[inds],
+            }
+        self.simulated = simulated_capacity
+
+    def get_installed_capacity(self, key, t):
+        # Drop tzinfo since capacity numbers only change slowly so that a couple
+        # of hours offset don't matter
+        ind = self.historic[key]["times"].searchsorted(t.replace(tzinfo=None))
+        # ind is now the point where we would need to insert the value if a new value
+        # at time t were known. At that point we had the previous capacity installed
+        # return that
+        # TODO: Gracefully deal with earlier times
+        return self.historic[key]["values"][ind-1]
+
+    def get_scale_factor(self, key, t):
+        try:
+            return self.simulated[key] / self.get_installed_capacity(key, t)
+        except KeyError:
+            if key not in ["hydro"]:
+                print(f"Missing key {key}")
+            return 1.
--- grid/grid.py
+++ grid/grid.py
@@ -2,16 +2,18 @@ import datetime
 import functools
 from scipy.interpolate import interp1d
 
+from grid.capacity import PowerCapacity
 
 
 @functools.lru_cache(maxsize=1000, typed=False)
-def battery_interaction(current_load, 
-                        current_production, 
-                        current_storage, 
-                        max_power=0.,     # GW
-                        max_capacity=0.,  # GWh
-                        efficiency=0.9,   # fraction, applied at storage time
-                       ):
+def battery_interaction(
+    current_load,
+    current_production,
+    current_storage,
+    max_power=0.0,  # GW
+    max_capacity=0.0,  # GWh
+    efficiency=0.9,  # fraction, applied at storage time
+):
     max_power *= 1000
     max_capacity *= 4 * 1000
     residual = current_production - current_load
@@ -22,10 +24,10 @@ def battery_interaction(current_load,
             if current_storage < -residual:
                 battery = current_storage
                 other = -residual - current_storage
-                current_storage = 0.
+                current_storage = 0.0
             else:
                 battery = -residual
-                other = 0.
+                other = 0.0
                 current_storage += residual
         # we cannot deal with this demand solely from batteries
         else:
@@ -33,14 +35,14 @@ def battery_interaction(current_load,
             if current_storage < max_power:
                 battery = current_storage
                 other = -residual - current_storage
-                current_storage = 0.
+                current_storage = 0.0
             else:
                 battery = max_power
                 other = -residual - max_power
                 current_storage -= max_power
     # we can store energy
-    else: 
-        other = 0.
+    else:
+        other = 0.0
         if residual > max_power:
             current_storage += max_power * efficiency
             battery = -max_power
@@ -53,27 +55,11 @@ def battery_interaction(current_load,
     return current_storage, battery, other
 
 
-def get_production(t, observed_data, sources, observed_capacity, parameters):
-    production = 0.
-    # production = 9000.
+def get_production(t, observed_data, sources, capacity):
+    production = 0.0
+
     for s in sources:
-        m = t.month
-        y = t.year
-        for i in range(6):
-            newm = m - i
-            newy = y
-            if newm < 1:
-                newy = y - 1
-                newm += 12
-            try:
-                obs_cap = observed_capacity[s][t.replace(year=newy, month=newm, day=1, minute=0, second=0, hour=0, tzinfo=None)]
-                scale_factor = parameters[s] / obs_cap
-                break
-            except KeyError:
-                # print(s, t)
-                continue
-        else:
-           scale_factor = 1.
+        scale_factor = capacity.get_scale_factor(s, t)
         production += observed_data[s][t] * scale_factor
 
     return production
@@ -86,42 +72,30 @@ def get_obs_capacity_fcts(observed_capacity):
         days = sorted(v.keys())
         values = [v[d] for d in days]
         days = [d.timestamp() for d in days]
-        obs_caps[k] = interp1d(days, values, fill_value='extrapolate')
+        obs_caps[k] = interp1d(days, values, fill_value="extrapolate")
     return obs_caps
 
 
-def run_simulation(observed_data, 
-                   observed_capacity, 
-                   parameters, 
-                   add_extra=False, 
-                   nsteps=100, 
-                   simstart=None, 
-                   extra_capacity=5., 
-                   start_extra_dt=-4*24*7, # one week
-                   extra_starts=[]):
-    sources = parameters['sources']
-    print(parameters.get('capacity', {}))
-    print(parameters.get('storage', {}))
+def run_simulation(
+    historic_data, historic_capacity, grid_configuration, nsteps=100, simstart=None
+):
+    """Simulate power generation for some grid_configuration
 
-    obs_caps = get_obs_capacity_fcts(observed_capacity)
-    
-    times = sorted(observed_data['solar'].keys())
-    times = [t for t in times if t > simstart.replace(tzinfo=t.tzinfo)]
+    Takes historical production and load data, scales it up (or down) to a given grid
+    configuration and returns the resulting load state of the power grid
+    """
 
-    extra_start_ts = []
-    extra_end_ts = []
-    for s, e in extra_starts:
-        extra_start_ts.append(times[s])
-        extra_end_ts.append(times[e])
-    if extra_start_ts:
-        next_extra_start = extra_start_ts.pop(0)
-        next_extra_end = extra_end_ts.pop(0)
-    else:
-        next_extra_start = times[-1]
-        next_extra_end = times[-1]
-    
-    current_storage = 0.
-    current_extra_prod = 0.
+    sources = grid_configuration["sources"]
+    print(grid_configuration.get("capacity", {}))
+    print(grid_configuration.get("storage", {}))
+
+    capacity = PowerCapacity(historic_capacity, grid_configuration['capacity'])
+
+    times = sorted(historic_data["solar"].keys())
+    if simstart:
+        times = [t for t in times if t > simstart.replace(tzinfo=t.tzinfo)]
+
+    current_storage = 0.0
     storages = []
     loads = []
     production = []
@@ -129,51 +103,36 @@ def run_simulation(observed_data,
     others = []
     oldt = None
     for t in times[:nsteps]:
-        if t >= next_extra_start:
-            current_extra_prod = extra_capacity * 1000.
-            try:
-                next_extra_start = extra_start_ts.pop(0)
-            except IndexError:
-                next_extra_start = times[-1]
-            # print('start', t, len(extra_start_ts), next_extra_start, len(extra_end_ts), next_extra_end)
-        if t >= next_extra_end:
-            current_extra_prod = 0.
-            try:
-                next_extra_end = extra_end_ts.pop(0)
-            except IndexError:
-                next_extra_end = times[-1]
-            # print('end', t, len(extra_start_ts), next_extra_start, len(extra_end_ts), next_extra_end)
-        if oldt:
-            deltat = t - oldt
-        else:
-            deltat = datetime.timedelta(minutes=15)
-        current_load = observed_data['load'][t]
-        
-        current_production = get_production(t, observed_data, sources, observed_capacity, parameters.get('capacity', {})) + current_extra_prod
+        current_load = historic_data["load"][t]
+
+        current_production = (
+            get_production(
+                t,
+                historic_data,
+                sources,
+                capacity,
+            )
+        )
 
         loads.append(current_load)
         production.append(current_production)
 
-        current_storage, bat, other = battery_interaction(current_load, current_production, current_storage, **parameters.get('storage', {}))
+        current_storage, bat, other = battery_interaction(
+            current_load,
+            current_production,
+            current_storage,
+            **grid_configuration.get("storage", {})
+        )
         battery.append(bat)
         others.append(other)
         storages.append(current_storage)
-        
-        
-        oldt = t
 
-    if add_extra:
-        return run_simulation(observed_data, observed_capacity, parameters, 
-                              add_extra=False, 
-                              nsteps=nsteps, 
-                              simstart=simstart, 
-                              extra_capacity=extra_capacity, 
-                              extra_starts=add_reserve_times(others, start_extra=start_extra_dt))
+        oldt = t
 
     return times, storages, loads, production, battery, others
 
 
-def add_reserve_times(others, start_extra=-4*24*7):
+def add_reserve_times(others, start_extra=-4 * 24 * 7):
     lack_of_capacity = []
     extra_needed = False
     cur_start = None
@@ -183,7 +142,7 @@ def add_reserve_times(others, start_extra=-4*24*7):
                 extra_needed = False
                 duration = i - cur_start
 
-                next_start = cur_start+start_extra
+                next_start = cur_start + start_extra
                 if next_start < 0:
                     next_start = 0
                 try:
@@ -191,16 +150,12 @@ def add_reserve_times(others, start_extra=-4*24*7):
                 except IndexError:
                     last_end = None
                 if last_end and next_start < last_end:
-                    lack_of_capacity[-1] = ((lack_of_capacity[-1][0], i))
+                    lack_of_capacity[-1] = (lack_of_capacity[-1][0], i)
                 else:
                     lack_of_capacity.append((next_start, i))
         else:
             if o > 0:
                 cur_start = i
                 extra_needed = True
-    
+
     return lack_of_capacity
-    
-    
-    
-        
